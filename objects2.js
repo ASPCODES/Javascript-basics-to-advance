823ef4a770cd4312ff6963616beae19c57cd08fe
ADITYA SINGH PARAMR <135034936+ASPCODES@users.noreply.github.com>
2023-11-02 12:52:51 +0530

    Add files via upload

---
+++ objects2.js
@@ -0,0 +1,105 @@
+// [[Prototype]]
+// : 
+// Object
+// constructor
+// : 
+// ƒ Object()
+// hasOwnProperty
+// : 
+// ƒ hasOwnProperty()
+// isPrototypeOf
+// : 
+// ƒ isPrototypeOf()
+// propertyIsEnumerable
+// : 
+// ƒ propertyIsEnumerable()
+// toLocaleString
+// : 
+// ƒ toLocaleString()
+// toString
+// : 
+// ƒ toString()
+// valueOf
+// : 
+// ƒ valueOf()
+// __defineGetter__
+// : 
+// ƒ __defineGetter__()
+// __defineSetter__
+// : 
+// ƒ __defineSetter__()
+// __lookupGetter__
+// : 
+// ƒ __lookupGetter__()
+// __lookupSetter__
+// : 
+// ƒ __lookupSetter__()
+// __proto__
+// : 
+// (...)
+// get __proto__
+// : 
+// ƒ __proto__()
+// set __proto__
+// : 
+// ƒ __proto__()
+
+
+
+// const tinderUser = new Object()   // this is a Singleton object.
+const tinderUser = {}   // This is non singleton object.
+
+tinderUser.id = "1445Dac"
+tinderUser.name = "Bhuvan Singh"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "ABC@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Aditya", 
+            lastname: "Singh"
+        }
+    }
+}
+
+ // console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+// const obj4 = Object.assign({}, obj1 , obj2, obj3)
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+// console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+]
+
+users [1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('id'));
+console.log(tinderUser.hasOwnProperty('name'));
+
+console.log(tinderUser.propertyIsEnumerable('id'));
